0b3a01d367b5372239e30ef2425e40d8927d4f52
张洪 <>
2020-10-19 17:13:42 +0800

    初始化

---
+++ BaseModule/Classes/ZHDefines.h
@@ -0,0 +1,16 @@
+//
+//  ZHDefines.h
+//  BaseModule
+//
+//  Created by 张洪 on 2020/10/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHDefines : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BaseModule/Classes/ZHDefines.m
@@ -0,0 +1,12 @@
+//
+//  ZHDefines.m
+//  BaseModule
+//
+//  Created by 张洪 on 2020/10/19.
+//
+
+#import "ZHDefines.h"
+
+@implementation ZHDefines
+
+@end
